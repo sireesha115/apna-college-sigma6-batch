0f9154b177a7a5ac1ce8a05d8df640d737b7904b
sireesha115 <sirismart115@gmail.com>
2025-04-18 19:48:49 +0530

    example: implement import and export keywords for module sharing

---
+++ NodeJS/example/math.js
@@ -0,0 +1,4 @@
+export const sum=(a,b)=>a+b;
+export const mul=(a,b)=>a*b;
+export const g=8.5;
+export const pi=3.14;
--- NodeJS/example/script.js
+++ NodeJS/example/script.js
@@ -1,4 +1,13 @@
-const fruit=require("./fruits");
-console.log(fruit);
-console.log(fruit[0]);
-console.log(fruit[0].name);
+// const fruit=require("./fruits");
+// console.log(fruit);
+// console.log(fruit[0]);
+// console.log(fruit[0].name);
+
+// import { mul, pi, sum } from "./math.js";
+import { generate } from "random-words";
+
+// console.log(sum(1, 2));
+// console.log(pi);
+// console.log(mul(4, 5));
+
+console.log(generate());
