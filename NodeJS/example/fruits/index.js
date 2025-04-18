7f2c95e651c194b5ef37e5dcef92bbdb12ab6513
sireesha115 <sirismart115@gmail.com>
2025-04-18 19:08:58 +0530

    example: demonstrate usage of module.exports with require

---
+++ NodeJS/example/fruits/index.js
@@ -0,0 +1,10 @@
+const apple=require("./apple");
+const mango=require("./mango");
+const orange=require("./orange");
+
+// console.log(apple);
+// console.log(mango);
+// console.log(orange);
+let fruits=[apple,mango,orange];
+
+module.exports=fruits;
---
+++ NodeJS/example/script.js
@@ -0,0 +1,4 @@
+const fruit=require("./fruits");
+console.log(fruit);
+console.log(fruit[0]);
+console.log(fruit[0].name);
